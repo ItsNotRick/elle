068cbab4e05c08ae2258ca51857f3dc7cee060d4
ItsNotRick <thompson.john.patrick@gmail.com>
2019-02-06 00:23:15 -0500

    Clone all script created

---
+++ scripts/clone_all.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/ItsNotRick/elle-mobile-game.git
+git clone https://github.com/ItsNotRick/elle-web-service.git
+git clone https://github.com/ItsNotRick/elle-web-portal.git
+
